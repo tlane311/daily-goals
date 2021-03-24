3c1da93ef77c027e4a977072b4ec6ba2e0de1334
Thomas Lane <tlane311@gmail.com>
2021-03-24 11:46:07 -0500

    fixed a bug with the db connection pool

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 import mysql from 'mysql'; //mysql driver
 import dotenv from 'dotenv'
-dotenv.config()
+dotenv.config({path: '../.env'})
 
 //we are going to use a connection pool for better performance
 //the connection pool allows to have X many connections sitting on standby
@@ -10,20 +10,30 @@ const poolConfig = {
     host: process.env.DBHOST,
     user: process.env.DBUSER,
     password: process.env.DBPASSWORD,
-    database: process.env.DATABASE
+    database: process.env.DATABASE,
+    connectionLimit: 15, //this is set by our db
 }
 
 
 
-//we are creating a promise here to export and use all of ours routes
-//Promises are eager so it is created the first time this code is run (not sure: either on server start or whenever the first route is called)
+//we are creating a promise here to export and use in all of ours routes
+//promises are eager so it is created the first time this code is run
 export const poolPromise = new Promise( (resolve, reject) => {
-    const pool = mysql.createPool(poolConfig);
-    pool.getConnection( (err, connection) => {
-        if (err) reject(err);   // failure throws err
-        resolve(connection);    // resolve returns connection object which can be then queried
-                                // e.g.
-                                // const pool = await poolPromise
-                                // pool.query('SELECT * FROM table1', (err, results) => {//do stuff})
-    })
-})
+    
+    try{
+        const pool = mysql.createPool(poolConfig);
+        resolve(pool);
+        
+        /* this was bad code from before
+        pool.getConnection( (err, connection) => {
+            if (err) reject({message: 'database error', error: err});   // failure throws err
+            resolve(connection);    // resolve returns connection object which can be then queried
+                                    // e.g.
+                                    // const pool = await poolPromise
+                                    // pool.query('SELECT * FROM table1', (err, results) => {//do stuff})
+        })*/
+    } catch (e){
+        console.log(e.message, e.error)
+    }
+
+});
