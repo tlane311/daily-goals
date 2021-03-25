028e93b645a6e2d65bf9f00f4a8ff090de201787
Thomas Lane <tlane311@gmail.com>
2021-03-25 17:14:23 -0500

    added goal creation feature to Sticky

--- src/components/sticky/Sticky.js
+++ src/components/sticky/Sticky.js
@@ -3,36 +3,34 @@ import '../../component-styles/sticky.css';
 
 import Task from '../task/task.js';
 
-//The Column component holds Tasks. Columns are held in the Column-View component which displays only a few columns at a time.
-//Entries are passed from the Column-View
-//Column must contain new task btn
+import goalManagement from '../../services/goalManagement.js';
 
-
-export default function Sticky({theList, theGoals, updateEntries, updateTask, deleteTask, deleteColumn}){
+export default function Sticky({theList, theGoals, token, updateApp}){
     const [newTask, setNewTask] = useState("");
+
+    const handleNewGoalCreation = e => {
+        goalManagement.create(token, theList['list_id'], newTask, theGoals.length+1);
+        updateApp();
+    }
 
     return (
         <div className="sticky" id="sticky">
             <h3> {theList['list_name']} </h3>
-            {/*<button onClick={() => {deleteColumn(id)}}>x</button>*/}
             <ul>
                 {theGoals.map(
-                    (entry,index) => 
+                    (goal,index) => 
                         <Task 
-                            name={entry.goal} 
-                            columnID={entry['list_id']}
-                            taskID={entry['goal_id']}
-                            completed={entry.status} 
-                            updateTask={updateTask} 
-                            deleteTask={deleteTask}
+                            name={goal.goal} 
+                            columnID={goal['list_id']}
+                            taskID={goal['goal_id']}
+                            completed={goal.status} 
+                            updateTask={()=>{}} 
+                            deleteTask={()=>{}}
                         />)}
             </ul>
             <span className="new-task">
                 <button 
-                    onClick={() => {
-                        updateEntries(newTask); //this will throw an error
-                        setNewTask(""); 
-                    }}>
+                    onClick={handleNewGoalCreation}>
                     +
                 </button> 
 
