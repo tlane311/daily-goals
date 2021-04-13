286a9957fac752d8604d81b70d77218cc2c6d6cc
Thomas Lane <tlane311@gmail.com>
2021-04-13 13:47:34 -0500

    split HighlightedText into a new file so that it could be imported to another component

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -1,6 +1,7 @@
 import '../../component-styles/task.css';
 
 import OrderButtons from './OrderButtons.js';
+import HighlightedText from './HighlightedText.js';
 import {useEffect, useState} from "react";
 import goalManagement from '../../services/goalManagement.js';
 
@@ -86,50 +87,4 @@ const X = () => {
             <div className="slant-down"/>
         </>
     )
-}
-
-const HighlightedText = ({text,color}) => {
-    const rgbValues = {
-        pink: {r: 244, g:62, b:223},
-        blue: {r:62, g: 223, b:244},
-        orange: {r:244, g:129, b:62},
-        green: {r:86, g:244, b:62}
-
-    }
-
-    const randomNumberBetween = (min,max) => {
-        const difference = max - min;
-        return difference*Math.random()+min;
-    }
-    
-    // This function takes in a rgb color and returns a background with two linear gradients
-    const background = ({r,g,b}) => {
-        const firstAngle = randomNumberBetween(99,103); // returns a random number between 103 and 99
-        const firstSpread = [randomNumberBetween(.7,1.5),2.4,5.8,93, randomNumberBetween(94,97), 98]
-        const firstDensity = [0,1.25,0.5,0.3,0.7,0]
-
-
-        const secondAngle = randomNumberBetween(273,277);
-        const secondSpread = [0,7.9,randomNumberBetween(13,20)];
-        const secondDensity = [0,0.3,0]
-
-        return `linear-gradient(${firstAngle}deg, rgba(${r},${g},${b},${firstDensity[0]}) ${firstSpread[0]}%,rgba(${r},${g},${b},${firstDensity[1]}) ${firstSpread[1]}%,rgba(${r},${g},${b},${firstDensity[2]}) ${firstSpread[2]}%,rgba(${r},${g},${b},${firstDensity[3]}) ${firstSpread[3]}%,rgba(${r},${g},${b},${firstDensity[4]}) ${firstSpread[4]}%,rgba(${r},${g},${b},${firstDensity[5]}) ${firstSpread[5]}%), linear-gradient(${secondAngle}deg, rgba(${r},${g},${b},${secondDensity[0]}) ${secondSpread[0]}%,rgba(${r},${g},${b},${secondDensity[1]}) ${secondSpread[1]}%,rgba(${r},${g},${b},${secondDensity[2]}) ${secondSpread[2]}%)`;
-    }
-
-    // If one of the colors if from a predetermined list make a marked element
-
-    if (color==="orange"){
-        return <mark style={ { background: background(rgbValues.orange) } }> {text} </mark>
-    }
-    if (color==="green"){
-        return <mark style={ { background: background(rgbValues.green) } }> {text} </mark>
-    }
-    if (color==="blue"){
-        return <mark style={ { background: background(rgbValues.blue) } }> {text} </mark>
-    }
-    if (color==="pink"){
-        return <mark style={ { background: background(rgbValues.pink) } }> {text} </mark>
-    }
-    
-    return text
 }
